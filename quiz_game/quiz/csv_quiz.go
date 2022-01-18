36181b039da68c2587a1fd19adfcc1906a08d52e
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-01-18 20:19:13 +0300

    Working implementation of CsvQuizReader

--- quiz_game/quiz/csv_quiz.go
+++ quiz_game/quiz/csv_quiz.go
@@ -1,12 +1,32 @@
 package quiz
 
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
 type CsvQuizStore struct {
+	reader *csv.Reader
 }
 
 func (s *CsvQuizStore) NextQuiz() (string, string, error) {
-	return "", "10", nil
+	record, err := s.reader.Read()
+	if err != nil {
+		return "", "", err
+	}
+	if len(record) != 2 {
+		return "", "", fmt.Errorf("wrong number of fields in a quiz record: %s. Expected 2 but got %d\n.", record, len(record))
+	}
+	return record[0], record[1], nil
 }
 
-func NewCsvQuizStore() *CsvQuizStore {
-	return &CsvQuizStore{}
+func NewCsvQuizStore(filePath string) (*CsvQuizStore, error) {
+	fd, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	// defer fd.Close()
+	store := CsvQuizStore{reader: csv.NewReader(fd)}
+	return &store, nil
 }
--- quiz_game/quiz/csv_quiz_test.go
+++ quiz_game/quiz/csv_quiz_test.go
@@ -1,17 +1,90 @@
 package quiz_test
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/ilia-tsyplenkov/gophercises/quiz_game/quiz"
 )
 
+func makeTestCsv(filename string, records [][]string) (filePath string, answers []string) {
+
+	fd, err := os.Create(filename)
+	if err == os.ErrExist {
+		os.Remove(filename)
+		fd, err = os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer fd.Close()
+	answers = make([]string, len(records))
+	w := csv.NewWriter(fd)
+	for i, record := range records {
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error preparing csv test file:", err)
+		}
+		answers[i] = record[1]
+	}
+	w.Flush()
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return fd.Name(), answers
+
+}
 func TestGetQuestionAndAnswerFromCsvQuizStore(t *testing.T) {
-	store := quiz.NewCsvQuizStore()
-	_, got, _ := store.NextQuiz()
-	want := "10"
-	if got != want {
-		t.Fatalf("expected %q but got %q\n", want, got)
+	testCases := []struct {
+		quizes  [][]string
+		answers []string
+	}{
+		{
+			[][]string{
+				{"10+10", "20"},
+				{"10-5", "5"},
+				{"15+10", "25"},
+			},
+			[]string{"20", "5", "25"},
+		},
 	}
 
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%dQuestions%dAnswers", len(tc.quizes), len(tc.answers))
+		t.Run(testName, func(t *testing.T) {
+			testFile := "testName" + ".csv"
+			makeTestCsv(testFile, tc.quizes)
+			store, err := quiz.NewCsvQuizStore(testFile)
+			if err != nil {
+				t.Errorf("unxpected error while creating CsvQuizStore: %s\n", err)
+			}
+			for _, want := range tc.answers {
+				_, got, err := store.NextQuiz()
+				if err != nil {
+					t.Fatalf("unexpected error during csv quiz reading: %s\n", err)
+				}
+				if got != want {
+					t.Errorf("expected %q but got %q\n", want, got)
+				}
+
+			}
+
+		})
+	}
+
+}
+
+func TestErrorGetCsvQuizWhenNoMoreRecord(t *testing.T) {
+	testFile, _ := makeTestCsv("fake.csv", nil)
+	quizStore, err := quiz.NewCsvQuizStore(testFile)
+	if err != nil {
+		t.Errorf("unxpected error while creating CsvQuizStore: %s\n", err)
+	}
+	_, _, err = quizStore.NextQuiz()
+	if err != io.EOF {
+		t.Errorf("expecting to have EOF error while reading from empty file, but got %q\n", err)
+	}
 }
