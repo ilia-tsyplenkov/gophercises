3044016965ede3d32d9432781e36d61a1480bb4b
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-01-20 12:53:18 +0300

    working main draft

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/ilia-tsyplenkov/gophercises/quiz_game/quiz"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ilia-tsyplenkov/gophercises/quiz_game/quiz"
+)
 
 type QuizGame struct {
 	quizStore   quiz.QuizReader
@@ -9,11 +15,12 @@ type QuizGame struct {
 
 func (g *QuizGame) CheckAnswers() (total, correct int) {
 	for {
-		_, rightAnswer, err := g.quizStore.NextQuiz()
+		question, rightAnswer, err := g.quizStore.NextQuiz()
 		if err != nil {
 			return
 		}
 		total++
+		fmt.Fprintf(os.Stdout, "%s: ", question)
 		answer, err := g.answerStore.NextAnswer()
 		if err != nil {
 			return
@@ -28,6 +35,19 @@ func (g *QuizGame) Launch() error {
 	return nil
 }
 
+var defaultFile string = "problems.csv"
+
 func main() {
 
+	quizStore, err := quiz.NewCsvQuizStore(defaultFile)
+	if err != nil {
+		log.Fatalln("error getting quiz data:", err)
+	}
+	answerStore, err := quiz.NewFileAnswerStore("")
+	if err != nil {
+		log.Fatalln("error getting file(file descriptor) for answers: ", err)
+	}
+	game := QuizGame{quizStore, answerStore}
+	totalAnswers, correctAnswers := game.CheckAnswers()
+	fmt.Printf("Quiz results: total - %d, correct -%d\n", totalAnswers, correctAnswers)
 }
