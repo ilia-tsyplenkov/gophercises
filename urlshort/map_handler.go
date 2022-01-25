5da2cc87f81e85119980b6a0fd76b1ed4ec47266
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-01-25 15:48:13 +0300

    Add fallback handler

--- urlshort/map_handler.go
+++ urlshort/map_handler.go
@@ -5,15 +5,20 @@ import (
 )
 
 type RedirectHandler struct {
-	Redirects map[string]string
+	redirects map[string]string
+	fallback  http.Handler
 }
 
+func NewRedirectHandler(redirects map[string]string, fallback http.Handler) RedirectHandler {
+	return RedirectHandler{redirects, fallback}
+}
 func (h RedirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for key, value := range h.Redirects {
+	for key, value := range h.redirects {
 		if key == r.URL.Path {
 			http.Redirect(w, r, value, http.StatusFound)
 			return
 
 		}
 	}
+	h.fallback.ServeHTTP(w, r)
 }
