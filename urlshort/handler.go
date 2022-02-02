40d7c13eca4b0b6b68b4dea58b9bf4bfefeb95a4
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-02-02 12:19:18 +0300

    Fix stupid logic for checking redirects

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -15,12 +15,10 @@ type RedirectHandler struct {
 }
 
 func (h RedirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for key, value := range h.redirects {
-		if key == r.URL.Path {
-			http.Redirect(w, r, value, http.StatusFound)
-			return
-
-		}
+	value, ok := h.redirects[r.URL.Path]
+	if ok {
+		http.Redirect(w, r, value, http.StatusFound)
+		return
 	}
 	h.fallback.ServeHTTP(w, r)
 }
