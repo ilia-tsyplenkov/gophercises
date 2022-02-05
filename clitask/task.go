865906642846f63d8894217765200b13da11608c
Ilya Tsyplenkov <ilya@mindsdb.com>
2022-02-05 14:11:15 +0300

    implement 'add', 'do', 'list' for mem storage

--- clitask/task.go
+++ clitask/task.go
@@ -1,13 +1,45 @@
 package clitask
 
+import "fmt"
+
 func GetTask() string {
 	return "write tests"
 }
 
-type MemStore struct {
-	tasks []string
+type Task struct {
+	name string
+	done bool
 }
 
-func (s *MemStore) TaskList() []string {
-	return s.tasks
+func (t Task) String() string {
+	return fmt.Sprintf("task: %q done: %v", t.name, t.done)
+}
+
+type MemStore struct {
+	todo []Task
+	done []Task
+}
+
+func (s *MemStore) AllTasks() []Task {
+	return s.todo
+}
+
+func (s *MemStore) ToDo() []Task {
+	return s.todo
+}
+
+func (s *MemStore) Add(task string) {
+	s.todo = append(s.todo, Task{name: task})
+}
+
+func (s *MemStore) Completed() []Task {
+	return s.done
+}
+
+func (s *MemStore) Do(id int) {
+	id--
+	task := s.todo[id]
+	s.todo = append(s.todo[:id], s.todo[id+1:]...)
+	task.done = true
+	s.done = append(s.done, task)
 }
--- clitask/task_test.go
+++ clitask/task_test.go
@@ -1,6 +1,7 @@
 package clitask
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -13,12 +14,95 @@ func TestGetTask(t *testing.T) {
 	}
 }
 
-func TestGetTaskList(t *testing.T) {
-	want := []string{"write test", "write code", "pass test"}
-	store := MemStore{want}
-	got := store.TaskList()
+func TestTaskToDoList(t *testing.T) {
+
+	testCases := []struct {
+		have []Task
+		want []Task
+	}{
+		{
+			have: nil, want: nil,
+		},
+		{
+			have: []Task{Task{name: "write test"}},
+			want: []Task{Task{name: "write test"}},
+		},
+		{
+			have: []Task{Task{name: "write test"}, Task{name: "write code"}},
+			want: []Task{Task{name: "write test"}, Task{name: "write code"}},
+		},
+	}
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%dtasks", len(tc.have))
+		t.Run(testName, func(t *testing.T) {
+			store := MemStore{todo: tc.have}
+			got := store.ToDo()
+			if !reflect.DeepEqual(got, tc.have) {
+				t.Fatalf("expected to have %v tasks list but got %v\n", tc.have, got)
+			}
+		})
+	}
+
+}
+
+func TestAddNewTask(t *testing.T) {
+	have := []Task{
+		Task{name: "write test", done: false},
+		Task{name: "write code", done: false},
+	}
+	newTask := Task{name: "pass test", done: false}
+
+	store := MemStore{have, nil}
+	want := make([]Task, len(have))
+	copy(want, have)
+	want = append(want, newTask)
+	store.Add(newTask.name)
+	got := store.ToDo()
 	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("expected to have %q tasks list but got %q\n", want, got)
+		t.Fatalf("expected to have %v tasks list but got %v\n", want, got)
+	}
+
+}
+
+func TestCompletedTaskList(t *testing.T) {
+	testCases := []struct {
+		have []Task
+		want []Task
+	}{
+		{
+			have: nil, want: nil,
+		},
+		{
+			have: []Task{Task{name: "write test", done: true}},
+			want: []Task{Task{name: "write test", done: true}},
+		},
+		{
+			have: []Task{Task{name: "write test", done: true}, Task{name: "write code", done: true}},
+			want: []Task{Task{name: "write test", done: true}, Task{name: "write code", done: true}},
+		},
+	}
+	for _, tc := range testCases {
+		testName := fmt.Sprintf("%dtasks", len(tc.have))
+		t.Run(testName, func(t *testing.T) {
+			store := MemStore{done: tc.have}
+			got := store.Completed()
+			if !reflect.DeepEqual(got, tc.have) {
+				t.Fatalf("expected to have %v tasks list but got %v\n", tc.have, got)
+			}
+		})
+	}
+
+}
+
+func TestDoTask(t *testing.T) {
+
+	have := []Task{Task{name: "write test", done: false}}
+	store := MemStore{have, nil}
+	store.Do(1)
+	want := []Task{Task{name: "write test", done: true}}
+	got := store.Completed()
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("expected to have %v completed tasks but got %v", want, got)
 	}
 
 }
